e81d34d2662089b9596124ca8339946a1fb3e032
patil-rudra <rudragoudpatil2005@gmail.com>
2025-11-27 18:52:10 +0530

    pillars

---
+++ pillars.py
@@ -0,0 +1,16 @@
+class Car:
+    def startEngine(self):
+        print("Engine started")
+
+    def stopEngine(self):
+        print("Engine stopped")
+
+c = Car()
+c.startEngine()
+
+class Bank:
+    def withdraw(self):
+        print("Withdraw processing...")
+
+b = Bank()
+b.withdraw()
